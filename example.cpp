a932dbe685dcfe8374eeb40b588d9db8a7810e28
Alexandru Bogdan <alexandru.bogdan@raptor-technologies.ro>
2019-04-30 19:40:19 +0300

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -5,18 +5,24 @@
 
 using namespace std;
 
+size_t MyFunction(string text)
+{
+    cout << "FunctionCallback says: " << text << endl;
+    return text.length();
+}
+
 class CallbackClass {
-    public:
-    CallbackClass() {}
-    ~CallbackClass() {}
-    
-    void CallbackMethod(std::string text) {cout << "Callback says: " << text << " !" << endl;}
-    void SlotMethod0(int i, std::string text) {cout << "Slot0: Signal " << i << " says: " << text << " !" << endl;}
-    void SlotMethod1(int i, std::string text) {cout << "Slot1: Signal " << i << " says: " << text << " !" << endl;}
+public:
+    void CallbackMethod(string text) {cout << "MethodCallback says: " << text << endl;}
+    void SlotMethod0(int i, string text) {cout << "Slot0: Signal " << i << " says: " << text << endl;}
+    void SlotMethod1(int i, string text) {cout << "Slot1: Signal " << i << " says: " << text << endl;}
 };
 
+
 CallbackClass callbackClass;
-MethodCallback<CallbackClass, void, string> myCallback(callbackClass, &CallbackClass::CallbackMethod);
+
+FunctionCallback<size_t, string> myFunctionCallback(&MyFunction);
+MethodCallback<CallbackClass, void, string> myMethodCallback(callbackClass, &CallbackClass::CallbackMethod);
 
 Signal<3, int, string> mySignal;
 MethodSlot<CallbackClass, int, string> mySlot0(callbackClass, &CallbackClass::SlotMethod0);
@@ -24,11 +30,11 @@ MethodSlot<CallbackClass, int, string> mySlot1(callbackClass, &CallbackClass::Sl
 
 int main()
 {
-    myCallback("Hello");
+    cout << myFunctionCallback("Hello Function") << " characters" << endl;
+    myMethodCallback("Hello Method");
 	
     mySignal.Connect(&mySlot0);
     mySignal.Connect(&mySlot1);
-    
     mySignal.Emit(1, "Goodbye");
     
     return 0;
